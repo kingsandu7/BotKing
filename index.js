c6a71ccc7b126f4cac93ebca6c2bfd4ccc7149b1
kingsandu7 <43926599+kingsandu7@users.noreply.github.com>
2019-04-24 01:23:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -28,23 +28,3 @@ bot.on('message', function (message){
         return ban.action(message)
     }
 });
-
-require('core-js');
-require('regenerator-runtime/runtime');
-require('@babel/register')({
-    presets: ['@babel/env', '@babel/react'],
-    plugins: [
-        '@babel/proposal-class-properties',
-        '@babel/transform-async-to-generator',
-        '@babel/proposal-object-rest-spread',
-        'dynamic-import-node',
-    ],
-});
-
-bot.on('message', msg => {
-    if (msg.content === "bonjour"){
-        msg.reply("Heureux de te revoir parmis nous.")
-    }
-    if (msg.content.match(/salut/i)) {
-            msg.reply('Je suis d\'accord avec toi.')
-    }
