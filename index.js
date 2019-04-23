1028682eed1abde9eca410ea3383f658ff1b33f7
kingsandu7 <43926599+kingsandu7@users.noreply.github.com>
2019-04-24 01:28:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.login('token'); NTcwMzc2NDMyOTM3ODYxMTUx.XL-XCQ.I5q88DU1f-P-d3kqbqH1ihoeWRo
+{ 
+  "token"  : "NTcwMzc2NDMyOTM3ODYxMTUx.XL-XCQ.I5q88DU1f-P-d3kqbqH1ihoeWRo",
+  "prefix" : "+"
+}
