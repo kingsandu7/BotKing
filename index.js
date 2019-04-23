9a1243e0016ed97277c186dd29e7c2408ea57e86
kingsandu7 <43926599+kingsandu7@users.noreply.github.com>
2019-04-24 01:16:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const prefix = ("?");
 
 bot.on('ready', () => {
     console.log('BOT');
-
+});
+    
 bot.on('guildMemberAdd', member => {
     member.createDM().then(channel => {
         return channel.send('Bienvenue sur le serveur' + member.displayName)
